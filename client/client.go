4405b94a691ef306e4e856c2cc28688896eb2033
limmiehoang <minhkhanhhoang2105@gmail.com>
2019-07-09 08:31:20 +0700

    Initial commit

---
+++ client/client.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"google.golang.org/grpc"
+	"io"
+	"log"
+
+	pb "github.com/limmiehoang/grpcdemo/student"
+)
+
+var (
+	serverAddr  = flag.String("s", "localhost:10000", "The server address in the format of host:port")
+	function = flag.String("func", "", "Choose a function to execute: find, create, or get")
+	id = flag.String("id", "", "Enter student id")
+	name = flag.String("name", "", "Enter student name")
+	keyword = flag.String("keyword", "", "Get students with this keyword")
+)
+
+func createStudent(client pb.StudentClient, createRequest *pb.CreateRequest)  {
+	resp, err := client.CreateStudent(context.Background(), createRequest)
+	if err != nil {
+		log.Fatalf("Could not create student: %v", err)
+	}
+	if !resp.Success {
+		log.Printf("Student id %s already exists.", createRequest.Student.Id)
+		return
+	}
+	log.Printf("A new student has been added with id: %s", resp.Id)
+}
+
+func findStudent(client pb.StudentClient, findRequest *pb.FindRequest)  {
+	resp, err := client.FindStudent(context.Background(), findRequest)
+	if err != nil {
+		log.Fatalf("Error on find student: %v", err)
+	}
+	if resp.Student == nil {
+		log.Printf("Could not find student with id: %s", findRequest.Id)
+		return
+	}
+	log.Printf("Found student: %v", resp.Student)
+}
+
+func getStudents(client pb.StudentClient, filter *pb.GetFilter) {
+	// calling the streaming API
+	stream, err := client.GetStudents(context.Background(), filter)
+	if err != nil {
+		log.Fatalf("Error on get students: %v", err)
+	}
+	for {
+		customer, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("%v.Students(_) = _, %v", client, err)
+		}
+		log.Printf("Customer: %v", customer)
+	}
+}
+
+func main()  {
+	flag.Parse()
+
+	conn, err := grpc.Dial(*serverAddr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewStudentClient(conn)
+
+	if *function == "" {
+		log.Fatalf("Please choose a function (find, create, or get) with -func flag.")
+	}
+
+	switch *function {
+	case "find":
+		if *id == "" {
+			log.Fatalf("Please enter student id with -id flag to find a student")
+
+		}
+		findRequest := &pb.FindRequest{Id: *id}
+		findStudent(client, findRequest)
+	case "create":
+		if *id == "" || *name == "" {
+			log.Fatalf("Please give both student id and student name with -id and -name flag to create a student")
+
+		}
+		createRequest := &pb.CreateRequest{
+			Student: &pb.Model{
+				Id:   *id,
+				Name: *name,
+			},
+		}
+		createStudent(client, createRequest)
+	case "get":
+		getFilter := &pb.GetFilter{Keyword: *keyword}
+		getStudents(client, getFilter)
+	default:
+		log.Fatalf("Sorry, function %s doesn't exist.", *function)
+	}
+
+
+
+
+
+}
---
+++ server/server.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"google.golang.org/grpc"
+	"io/ioutil"
+	"log"
+	"net"
+	"strings"
+
+	pb "github.com/limmiehoang/grpcdemo/student"
+)
+
+var (
+	port = flag.Int("p", 10000, "The server port")
+	jsonDBFile = flag.String("db", "data/student_db.json", "A json file containing a list of features")
+)
+
+type server struct {
+	savedStudents map[string]*pb.Model
+}
+
+func (s *server) FindStudent(ctx context.Context, in *pb.FindRequest) (*pb.FindResponse, error) {
+	student, exists := s.savedStudents[in.Id]
+	if !exists {
+		return &pb.FindResponse{Student: nil}, nil
+	}
+	return &pb.FindResponse{Student: student}, nil
+}
+
+func (s *server) CreateStudent(ctx context.Context, in *pb.CreateRequest) (*pb.CreateResponse, error) {
+	if _, exists := s.savedStudents[in.Student.Id]; exists {
+		return &pb.CreateResponse{
+			Id: "",
+			Success: false,
+		}, nil
+	}
+	s.savedStudents[in.Student.Id] = in.Student
+	return &pb.CreateResponse{
+		Id: in.Student.Id,
+		Success: true,
+	}, nil
+}
+
+func (s *server) GetStudents(filter *pb.GetFilter, stream pb.Student_GetStudentsServer) error {
+	for _, student := range s.savedStudents {
+		if filter.Keyword != "" {
+			if !strings.Contains(student.Name, filter.Keyword) {
+				continue
+			}
+		}
+		if err := stream.Send(student); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *server) loadStudents(filePath string)  {
+	var data []byte
+	var err error
+	data, err = ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Fatalf("Failed to load default students: %v", err)
+	}
+	var studentArr []*pb.Model
+	if err := json.Unmarshal(data, &studentArr); err != nil {
+		log.Fatalf("Failed to load default students: %v", err)
+	}
+	for _, student := range studentArr {
+		s.savedStudents[student.Id] = student
+	}
+}
+
+func newServer() *server {
+	s := &server{savedStudents: make(map[string]*pb.Model)}
+	s.loadStudents(*jsonDBFile)
+	return s
+}
+
+func main()  {
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterStudentServer(grpcServer, newServer())
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
